acdf6883014864ab11c32a12f4656151445b883e
snehalyelmati <snehalyelmati@gmail.com>
2022-01-09 12:38:39 +0530

    tmux scripts and config update

--- backup.sh
+++ backup.sh
@@ -8,14 +8,15 @@ cp ~/.config/alacritty/alacritty.yml ./.config/alacritty
 cp ~/.config/i3/* ./.config/i3/
 cp ~/.config/picom/* ./.config/picom/
 cp ~/.config/ranger/* ./.config/ranger/
-cp -R /usr/share/fonts/TTF/* ./fonts/TTF/
-cp -R /usr/share/fonts/OTF/* ./fonts/OTF/
+rm -r ./fonts/TTF; mkdir ./fonts/TTF; cp -R /usr/share/fonts/TTF/* ./fonts/TTF/
+rm -r ./fonts/OTF; mkdir ./fonts/OTF; cp -R /usr/share/fonts/OTF/* ./fonts/OTF/
 cp /usr/share/fonts/JetBrainsMono/* ./fonts/JetBrainsMono/
 cp /usr/bin/blurlock .
 cp /usr/bin/i3exit .
 cp ~/.p10k.zsh .
 cp ~/.config/redshift/* ./.config/redshift/
 cp ~/.tmux.conf .
+rm -r ./.scripts; mkdir .scripts; cp ~/.scripts/* ./.scripts/
 "
 
 cp ~/.vimrc .
@@ -24,18 +25,21 @@ cp ~/.config/alacritty/alacritty.yml ./.config/alacritty
 cp ~/.config/i3/* ./.config/i3/
 cp ~/.config/picom/* ./.config/picom/
 cp ~/.config/ranger/* ./.config/ranger/
-cp -R /usr/share/fonts/TTF/* ./fonts/TTF/
-cp -R /usr/share/fonts/OTF/* ./fonts/OTF/
+rm -r ./fonts/TTF; mkdir ./fonts/TTF; cp -R /usr/share/fonts/TTF/* ./fonts/TTF/
+rm -r ./fonts/OTF; mkdir ./fonts/OTF; cp -R /usr/share/fonts/OTF/* ./fonts/OTF/
 cp /usr/share/fonts/JetBrainsMono/* ./fonts/JetBrainsMono/
 cp /usr/bin/blurlock .
 cp /usr/bin/i3exit .
 cp ~/.p10k.zsh .
 cp ~/.config/redshift/* ./.config/redshift/
 cp ~/.tmux.conf .
+rm -r ./.scripts; mkdir .scripts; cp ~/.scripts/* ./.scripts/
 
 git status
 
-echo "Done..."
+echo ""
+echo "Done."
+echo "Updating dotfiles management to stow..."
 }
 
 echo "time:" 
