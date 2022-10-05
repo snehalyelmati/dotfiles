b637b3b9fcee02299ee4c478aec0e8df0073f806
Snehal Reddy Yelmati <snehalyelmati@gmail.com>
2022-10-05 19:13:43 +0530

    Neovim minor config update

--- .config/nvim/lua/user/colorscheme.lua
+++ .config/nvim/lua/user/colorscheme.lua
@@ -1,9 +1,13 @@
 -- vim.cmd [[colorscheme default]]
 
-local colorscheme = "gruvbox"
--- local colorscheme = "gruvbox-material"
+-- local colorscheme = "gruvbox"
+local colorscheme = "gruvbox-material"
 -- vim.g.gruvbox_contrast_dark = "hard"
-vim.cmd("let g:gruvbox_contrast_dark = 'soft'")
+-- vim.cmd("let g:gruvbox_contrast_dark = 'soft'")
+
+--[[ vim.g.catppuccin_flavour = "macchiato" -- latte, frappe, macchiato, mocha ]]
+--[[ require("catppuccin").setup() ]]
+--[[ local colorscheme = "catppuccin" ]]
 
 local status_ok, _ = pcall(vim.cmd, "colorscheme " .. colorscheme)
 
